ff3b3c2feec94cbcdb48f841e458ed622802e649
CarlosIvan1997 <carlosipm69@gmail.com>
2017-09-17 10:35:52 -0500

    Iniciando clase 2

---
+++ app/src/main/java/poclin/carlos/clase2/Segunda_app.java
@@ -0,0 +1,18 @@
+package poclin.carlos.clase2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Segunda_app extends AppCompatActivity {
+
+    private TextView mTvTextPrueba;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda_app);
+
+    }
+}
+
