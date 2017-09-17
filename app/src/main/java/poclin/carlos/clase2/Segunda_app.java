c6f7b3e104d311c7e0f417fbdd98d3d0d16a40ed
CarlosIvan1997 <carlosipm69@gmail.com>
2017-09-17 13:04:10 -0500

    relaative layout

--- app/src/main/java/poclin/carlos/clase2/Segunda_app.java
+++ app/src/main/java/poclin/carlos/clase2/Segunda_app.java
@@ -2,6 +2,7 @@ package poclin.carlos.clase2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class Segunda_app extends AppCompatActivity {
@@ -12,6 +13,27 @@ public class Segunda_app extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segunda_app);
+
+        mTvTextPrueba = (TextView) findViewById(R.id.tv_texto_prueba);
+
+        String myString = getString(R.string.login_username);
+        Integer myInteger = getResources().getInteger(R.integer.my_integer);
+
+        int myIntegerArray [] = getResources().getIntArray(R.array.my_integer_array);
+
+        String myStringArray [] = getResources().getStringArray(R.array.my_string_array);
+
+        Boolean myBool = getResources().getBoolean(R.bool.my_bool);
+
+        for (int i = 0; i < myIntegerArray.length; i++){
+            Log.v("integer", ""+myIntegerArray[i]);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (String item : myStringArray){
+            sb.append(item+"\n");
+        }
+
+        mTvTextPrueba.setText(sb.toString());
 
     }
 }
